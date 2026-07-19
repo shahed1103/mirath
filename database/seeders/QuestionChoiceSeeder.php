e995aca4f0a24759a9299d32b8632e1aebf5fe46
shahed1103 <raniasol666@gmail.com>
2026-07-19 16:02:49 +0300

    Update QuestionChoiceSeeder.php

--- database/seeders/QuestionChoiceSeeder.php
+++ database/seeders/QuestionChoiceSeeder.php
@@ -13,35 +13,18 @@ class QuestionChoiceSeeder extends Seeder
      */
     public function run(): void
     {
-        $question_ids = [1,1,1,1,
-                        2,2,2,2,
-                        3,3,3,3,
-                        4,4,4,4,
-                        5,5,5,5,
-                        6,6,6,6,
-                        7,7,7,7,
-                        8,8,8,8,
-                        9,9,9,9,
-                        10,10,10,10,
-                        11,11,11,11,
-                        12,12,12,12,
-                        13,13,13,13,
-                        14,14,14,14,
-                        15,15,15,15,
-                        16,16,16,16,
-                        17,17,17,17,
-                        18,18,18,18,
-                        19,19,19,19,
-                        20,20,20,20];
         $choice_text = ['التعامل مع المخالفين' , 'دلائل أصول الإسلام' , 'بوصلة المصلح' , 'مصادر التلقي والمعرفة' ];
         $is_correct = [false , false , false ,true];
 
-        for ($i=0; $i < 80 ; $i++) {
-            QuestionChoice::query()->create([
-           'question_id'=> $question_ids[$i],
-           'choice_text' => $choice_text[$i % count($choice_text)],
-           'is_correct' => $is_correct[$i % count($is_correct)]
-            ]); }
+        for ($i=1; $i <= 80 ; $i++) {
+            for($j=0; $j < 4 ; $j++) {
+                QuestionChoice::query()->create([
+                    'question_id'=> $i,
+                    'choice_text' => $choice_text[$j % count($choice_text)],
+                    'is_correct' => $is_correct[$j % count($is_correct)]
+                ]); 
+            }
+        }
     }
 }
 
